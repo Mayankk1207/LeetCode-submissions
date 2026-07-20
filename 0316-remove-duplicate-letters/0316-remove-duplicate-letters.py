b09ce59fc866a8d5813f2b72562668d0193cf204
Mayank kumar <mayank.usar@gmail.com>
2026-07-20 14:03:59 +0530

    Time: 6 ms (25.65%), Space: 12.3 MB (79.69%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        count = Counter(s)
+        stack = []
+        seen = set()
+
+        for ch in s:
+            count[ch] -= 1
+
+            if ch in seen:
+                continue
+
+            while stack and stack[-1] > ch and count[stack[-1]] > 0:
+                seen.remove(stack.pop())
+
+            stack.append(ch)
+            seen.add(ch)
+
+        return "".join(stack)
+            
